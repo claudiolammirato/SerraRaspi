2760368f56e744d94bd6d0b49a97a12ac93d7498
claudiolammirato <claudio.lammirato@gmail.com>
2021-01-17 20:07:54 +0100

    init

---
+++ ads1115.js
@@ -0,0 +1,18 @@
+const ADS1115 = require('ads1115')
+ 
+const i2c = require('i2c-bus')
+i2c.openPromisified(1).then(async (bus) => {
+  const ads1115 = await ADS1115(bus)
+  // ads1115.gain = 1
+ 
+  for (let i = 0; i < 5; i++) {
+    let value0 = await ads1115.measure('0+GND')
+    let value1 = await ads1115.measure('1+GND')
+    let value2 = await ads1115.measure('2+GND')
+    let value3 = await ads1115.measure('3+GND')
+    console.log('Valore A0: '+value0)
+    console.log('Valore A1: '+value1)
+    console.log('Valore A2: '+value2)
+    console.log('Valore A3: '+value3)
+  }
+})
--- tempread.js
+++ tempread.js
@@ -1,1 +1,13 @@
-cla
+var sensor = require("node-dht-sensor");
+ 
+sensor.read(22, 27, function(err, temperature, humidity) {
+  if (!err) {
+    console.log(`Sensore interno temp: ${temperature}°C, humidity: ${humidity}%`);
+  }
+});
+
+sensor.read(22, 17, function(err, temperature, humidity) {
+    if (!err) {
+      console.log(`Sensore esterno temp: ${temperature}°C, humidity: ${humidity}%`);
+    }
+  });
