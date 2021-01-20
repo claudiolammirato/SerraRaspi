590cbda31ac8e3f1e1694d3e7e31c15b455100be
claudiolammirato <claudio.lammirato@gmail.com>
2021-01-20 21:50:03 +0100

    init

--- test/mariadb.js
+++ test/mariadb.js
@@ -11,3 +11,21 @@ var mysql = require('mysql');
       if (err) throw err;
       console.log("Connected!");
     });
+
+    con.query("CREATE DATABASE IF NOT EXISTS serradb;", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+    con.query("USE serradb;", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+
+
+    con.query("CREATE TABLE IF NOT EXISTS parameters_table (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR(50) NULL DEFAULT '', `value` VARCHAR(100) NULL DEFAULT '');", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+    con.end();
+
+
